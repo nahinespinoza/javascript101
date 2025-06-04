bd9a583ebd29fc728e11a91868300016dfc8b1cb
nahinespinoza <136995519+nahinespinoza@users.noreply.github.com>
2025-06-04 15:12:33 +0000

    Cambios1

--- src/file01.js
+++ src/file01.js
@@ -3,6 +3,7 @@
  * El modo estricto ayuda a detectar errores comunes y mejora la seguridad del código.
  * Referencia: https://javascript.info/strict-mode
  */
+"use strict";
 
 
 
@@ -10,15 +11,17 @@
  * Importe la función setTitle desde el archivo functions.js.
  * Referencia: https://javascript.info/import-export
  */
-
-
+import { setTitle } from "./functions.js";
 
 /** 
  * Establezca la variable local pageTitle con el título de la página.
  * Declare la variable local pageId con el ID del elemento HTML donde se mostrará el título.
  * Referencia: https://javascript.info/variables
  */
+let pageTitle;
 pageTitle = "Datos comparativos de frameworks JavaScript modernos";
+let pageId;
 pageId = "title";
 
 // Llama a la función setTitle para establecer el contenido del elemento HTML con el ID especificado
+setTitle(pageId,pageTitle);
--- src/file02.js
+++ src/file02.js
@@ -19,24 +19,38 @@ const frameworks = [
 /**
  * 1. Itere el arreglo frameworks.
  * Referencia: https://javascript.info/array#loops
- * 
- * 2. En cada iteración, toma una cadena del arreglo y divídela en partes usando el separador "|".
- *    Referencia: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split
- *   
- *    Extrae los valores: nombre del framework, fecha de lanzamiento, número de usuarios y porcentaje de popularidad.
- *    Referencia: https://javascript.info/destructuring-assignment   
- * 
- *    Crea un objeto frameworkObj con las siguientes propiedades:
- *    - frameworkName
- *    - releaseDate
- *    - usersName
- *    - popularityPercentage
- *    Referencia: https://developer.mozilla.org/es/docs/Web/JavaScript/Guide/Working_with_Objects
- * 
- *    Asegúrate de eliminar los espacios en blanco alrededor de cada valor usando `.trim()`.
- *    Referencia: https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/String/trim
- * 
- *    Llama a la función addRow pasando el objeto frameworkObj y el ID del cuerpo de la tabla "data-frameworks",
- *    para agregar una nueva fila con estos datos al cuerpo de la tabla HTML.
  */
+for (let i = 0; i < frameworks.length; i++) {
+  const str = frameworks[i];
+  const words = str.split("|");
+  let [name, date, num_users, popularidad] = words;
+  const objeto = {
+    frameworkName: name.trim(),
+    releaseDate: date.trim(),
+    usersName: num_users.trim(),
+    popularityPercentage: popularidad.trim()
+  };
+  addRow(objeto,"data-frameworks");
+}
+
+/**
+* 2. En cada iteración, toma una cadena del arreglo y divídela en partes usando el separador "|".
+*    Referencia: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split
+*   
+*    Extrae los valores: nombre del framework, fecha de lanzamiento, número de usuarios y porcentaje de popularidad.
+*    Referencia: https://javascript.info/destructuring-assignment   
+* 
+*    Crea un objeto frameworkObj con las siguientes propiedades:
+*    - frameworkName
+*    - releaseDate
+*    - usersName
+*    - popularityPercentage
+*    Referencia: https://developer.mozilla.org/es/docs/Web/JavaScript/Guide/Working_with_Objects
+* 
+*    Asegúrate de eliminar los espacios en blanco alrededor de cada valor usando `.trim()`.
+*    Referencia: https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/String/trim
+* 
+*    Llama a la función addRow pasando el objeto frameworkObj y el ID del cuerpo de la tabla "data-frameworks",
+*    para agregar una nueva fila con estos datos al cuerpo de la tabla HTML.
+*/
 
